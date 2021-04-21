2a725edb4a56220267b8eccb9f6f2112ee503486
jadyel sousa <jadyelbatera@gmail.com>
2021-04-21 11:06:08 -0300

    page main

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,33 +2,38 @@ import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 import './Login.css';
 import api from '../services/api';
-
-async function responseFacebook(response) {
-
-    const dados = await api.post('/user', {
-        response,
-    });
-
-    console.log(dados);
-
-}
+import logo from '../assets/logo.svg';
 
 const componentClicked = (response) => {
     console.log('yes');
 }
 
-export default function Login() {
+export default function Login({history}) {
+
+    async function responseFacebook(response) {
+
+        const dados = await api.post('/user', {
+            response,
+        })
+        
+        if(dados){
+            const { _id } = dados.data;s
+            history.push(`/main/${_id}`);
+        }  
+    }
     return (
 
         <div className="login-container">
 
             <form>
+            <img src={logo} alt='Musmeet'/>
                 <FacebookLogin
                     appId="164755398795594"
                     autoLoad={false}
                     fields="name,email,picture"
                     onClick={componentClicked}
-                    callback={responseFacebook} />
+                    callback={responseFacebook}
+                    textButton={"Login com facebook"}/>
 
             </form>
 
