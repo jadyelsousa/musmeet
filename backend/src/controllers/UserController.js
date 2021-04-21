a77644aa78d0c3c478829b15a477ae58be10dbec
jadyel sousa <jadyelbatera@gmail.com>
2021-04-21 11:07:58 -0300

    page login

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -19,6 +19,8 @@ module.exports = {
     async store(req, res) {
         const { name, email, picture } = req.body.response;
 
+        console.log(req.body.response);
+
         const userExists = await User.findOne({ name: name });
 
         if (userExists) {
@@ -33,7 +35,5 @@ module.exports = {
         })
 
         return res.json(user);
-
-
     }   
 }
